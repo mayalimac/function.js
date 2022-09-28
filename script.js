c1eb0cea70aad2c2696edd5d44fbe5ef4a8b53ab
Mayara Lima <91838870+mayalimac@users.noreply.github.com>
2022-09-28 10:36:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function primeiraFuncao() {
+    
+console.log("Hello world das funções");
+
+
+
+}
+
+primeiraFuncao();
+
+function dizerNome(nome) {
+
+    console.log(" O nome é: "+ nome);
+}
+
+dizerNome("Mayara");
+dizerNome("Tamyres");
+dizerNome("Maysa");
+
+var nomeDoBancoDeDados = "Familia";
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a,b){
+    var soma = a+b;
+    return soma;
+}
+
+var somaUm = soma(2,5);
+
+console.log(somaUm);
+
+var somaDois = soma(5, 5);
+
+console.log(somaDois);
+
+console.log(soma(4, 5));
+
